abd7e993ca87d81de21bbd125b5cb46d4b6c77b8
Sasha Fu <mariafu0801@gmail.com>
2021-04-15 15:38:23 +0800

    picturePlayer.scss correct detail

--- src/components/homepage/picturePlayer/PicturePlayer.jsx
+++ src/components/homepage/picturePlayer/PicturePlayer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./PicturePlayer.scss";
+import styles from "./PicturePlayer.module.scss";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
@@ -8,8 +8,8 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 function PicturePlayer() {
   return (
     <div>
-      <div className="picture"></div>
-      <div className="arrowWrapper">
+      <div className={styles.picture}></div>
+      <div className={styles.arrowWrapper}>
         <div>
           <FontAwesomeIcon icon={faChevronLeft} />
         </div>
@@ -17,16 +17,17 @@ function PicturePlayer() {
           <FontAwesomeIcon icon={faChevronRight} />
         </div>
       </div>
-      <div className="dotWrapper">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
+      <div className={styles.dotWrapper}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-      <div className="contentWrapper">
+      <div className={styles.contentWrapper}>
         <div>
-          微碧愛點餐 <br />
+          <h1>微碧愛點餐</h1> <br />
           <p> 點餐不用等，取餐就要快狠準</p>
         </div>
         <button>立即搜尋餐廳</button>
